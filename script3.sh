98c2600b933a14404f8c186eef49e837a2282321
Muhammad Danial Khan <72875993+DanialKhan800@users.noreply.github.com>
2022-07-08 01:27:06 +0500

    Create script3.sh

---
+++ script3.sh
@@ -0,0 +1,10 @@
+%target_hostname%:%target_download_dir% 
+
+
+%arch_tests_dir%/tcc_tools_tests.tar.gz 
+
+
+%tccdev_checkout_dir%/ci/run_robot_tests.py 
+%tccdev_checkout_dir%/ci/run_platform_tests.py 
+%tccdev_checkout_dir%/ci/remove_tcc_files.sh 
+
