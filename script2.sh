4538239b141164a5bf5b31b37d38bcef803d183d
Muhammad Danial Khan <72875993+DanialKhan800@users.noreply.github.com>
2022-07-08 01:25:12 +0500

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -6,3 +6,8 @@ set -e
 rm -rf %host_test_path% 
 mkdir %host_test_path% 
 
+
+# Copy files 
+cp %tccdev_checkout_dir%/ci/run_robot_tests.py %host_test_path% 
+cp %arch_tests_dir%/tcc_tools_tests.tar.gz %host_test_path% 
+
